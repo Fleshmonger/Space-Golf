049a73759b8748104764d35749eae7032bb689c7
Fleshmonger <fleshmonger@gmail.com>
2016-10-13 22:41:58 +0200

    Added no tilt option

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,15 +3,40 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
+    public bool tilt = false;
     public float speed = 100f;
+    public GameObject pitch;
+
+    public void ToggleTilt()
+    {
+        tilt = !tilt;
+        ResetRotation();
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.identity;
+        pitch.transform.rotation = Quaternion.identity;
+    }
 
     public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ToggleTilt();
+        }
         if (Input.GetMouseButton(0))
         {
             Vector2 pos = Input.mousePosition;
             Vector2 offset = pos - Center();
-            Rotate(offset);
+            if (tilt)
+            {
+                Rotate(offset);
+            }
+            else
+            {
+                RotateNoTilt(offset);
+            }
         }
     }
 
@@ -24,6 +49,12 @@ public class CameraController : MonoBehaviour
     public void Rotate(Vector2 offset)
     {
         Vector3 axis = new Vector3(-offset.y, offset.x, 0);
-        gameObject.transform.Rotate(axis, Time.deltaTime * speed * (offset.magnitude / Center().magnitude));
+        transform.Rotate(axis, Time.deltaTime * speed * (offset.magnitude / Center().magnitude));
+    }
+
+    public void RotateNoTilt(Vector2 offset)
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * speed * (offset.x / Center().magnitude));
+        pitch.transform.Rotate(Vector3.right, Time.deltaTime * speed * (-offset.y / Center().magnitude));
     }
 }
