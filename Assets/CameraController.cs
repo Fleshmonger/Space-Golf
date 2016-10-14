62081363aeb84780bbf1839d1bd5907011140aa7
Fleshmonger <fleshmonger@gmail.com>
2016-10-14 07:48:06 +0200

    Added camera moving + character moving

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,14 +3,31 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
+    private bool grounded = true, overTheShoulder = true;
+    private Vector3[] cameraPositions;
+
     public bool tilt = false;
-    public float speed = 100f;
+    public float cameraRotateSpeed = 120f, launchSpeed = 20f;
+    public Camera camera;
     public GameObject pitch;
 
     public void ToggleTilt()
     {
         tilt = !tilt;
         ResetRotation();
+    }
+
+    public void ToggleCameraPos()
+    {
+        overTheShoulder = !overTheShoulder;
+        if (overTheShoulder)
+        {
+            camera.transform.localPosition = new Vector3(0f, 2.5f, -5f);
+        }
+        else
+        {
+            camera.transform.localPosition = Vector3.zero;
+        }
     }
 
     public void ResetRotation()
@@ -25,6 +42,10 @@ public class CameraController : MonoBehaviour
         {
             ToggleTilt();
         }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ToggleCameraPos();
+        }
         if (Input.GetMouseButton(0))
         {
             Vector2 pos = Input.mousePosition;
@@ -38,6 +59,20 @@ public class CameraController : MonoBehaviour
                 RotateNoTilt(offset);
             }
         }
+        if (Input.GetMouseButtonDown(1) && grounded)
+        {
+            Vector3 direction = pitch.transform.forward;
+            Rigidbody body = GetComponent<Rigidbody>();
+            body.velocity = direction.normalized * launchSpeed;
+            grounded = false;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.velocity = Vector3.zero;
     }
 
     public Vector2 Center()
@@ -49,12 +84,12 @@ public class CameraController : MonoBehaviour
     public void Rotate(Vector2 offset)
     {
         Vector3 axis = new Vector3(-offset.y, offset.x, 0);
-        transform.Rotate(axis, Time.deltaTime * speed * (offset.magnitude / Center().magnitude));
+        transform.Rotate(axis, Time.deltaTime * cameraRotateSpeed * (offset.magnitude / Center().magnitude));
     }
 
     public void RotateNoTilt(Vector2 offset)
     {
-        transform.Rotate(Vector3.up, Time.deltaTime * speed * (offset.x / Center().magnitude));
-        pitch.transform.Rotate(Vector3.right, Time.deltaTime * speed * (-offset.y / Center().magnitude));
+        transform.Rotate(Vector3.up, Time.deltaTime * cameraRotateSpeed * (offset.x / Center().magnitude));
+        pitch.transform.Rotate(Vector3.right, Time.deltaTime * cameraRotateSpeed * (-offset.y / Center().magnitude));
     }
 }
